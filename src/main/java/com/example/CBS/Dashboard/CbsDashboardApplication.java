553ff5053a247ea2e1393f3fb048994b4f2c2dcd
Aziz Rahman Zazai <rahman.zazai@centralbank.gov.af>
2025-11-16 09:35:16 +0430

    first commit

---
+++ src/main/java/com/example/CBS/Dashboard/CbsDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.CBS.Dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbsDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbsDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CBS/Dashboard/CbsDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CBS.Dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbsDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
